864d5e79fe3b2f7b7fb69edbaa6922bd6ea2b470
unknown <joaoferreira6173@digitalcollege.internal>
2025-05-26 22:03:14 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Sun } from "phosphor-react"
+
+function App() {
+  return (
+    <div>
+      <header className="bg-red-300 flex items-center">
+        <Sun size={25} />
+      </header>
+    </div>
+  )
+}
+
+export default App
