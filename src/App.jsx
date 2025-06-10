45096a8ea0450535ff1300bbb30e378857ece04d
unknown <joaoferreira6173@digitalcollege.internal>
2025-06-09 22:03:12 -0300

    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { InstagramLogo, LinkedinLogo, Sun, WhatsappLogo } from "phosphor-react"
-import Botao from "./components/Botao/botao";
+import { FacebookLogo, InstagramLogo, LinkedinLogo, Sun, WhatsappLogo } from "phosphor-react"
+import Botao from "./components/Botao/Botao";
+import { useState } from "react";
 
 function App() {
-  return (
-    <main className="h-screen w-full overflow-hidden">
+ const [darkMode, setDarkMode] = useState(false);
+  
+ function handleChangeMode() {
+  // O operador de navegação `!` inverte o valor booleano de `darkMode`.
+  // Se darkMode for ´true´, ele se tornará `false`, e vice-versa.
+  // Isso é usado para alternar entre os modos claro e escuro
+  setDarkMode(!darkMode);
+ }
+
+ //if e else
+ // if (darkMode) {
+ // } else {
+ //
+ // }
+
+ // darkMode === true ? algumacoisa : outracoisa
+
+ return (
+    <main className={`h-screen w-full overflow-hidden ${darkMode === true ? "bg-slate-900" : "bg-white" } transition-all`}>
       <header className="flex justify-end p-4">
-        <button className="p-2 hover:bg-gray-200 text-black rounded-lg cursor-pointer transition duration-300">
+        <button 
+        onClick={handleChangeMode}
+        className="p-2 hover:bg-gray-200 text-black rounded-lg cursor-pointer transition duration-300">
           <Sun size={20} />
         </button>
       </header>
@@ -13,12 +33,12 @@ function App() {
 
       <main className="h-full w-full flex justify-center items-center overflow-hidden">
         <section className="border rounded-lg border-gray-500 min-w-96 flex items-center justify-center gap-4 p-6 flex-col">
-          <img src="https://github.com/jp-dev18.png" alt="foto do usuário" className="border-4 border-gray-300 w-32 h-32 rounded-full" />
+          <img src="https://github.com/jp-dev18.png" alt="foto do usuário" className="border-4 border-gray-300 w-32 h-32 rounded-full hover:scale-350 transition-all" />
 
-          <h1 className="font-bold text-2xl">Pedro Maciel</h1>
-          <p className="text-slate-400">Desenvolvedor Full-Stack</p>
+          <h1 className={`font-bold text-2xl ${darkMode ? "text-white" : "text-black"}`}>Pedro Maciel</h1>
+      <p className={`text-slate-400 ${darkMode ? "text-white" : "text-black"}`}>Desenvolvedor Full-Stack</p>
 
-          <section className="w-full space-y-4">
+          <section className={`w-full space-y-4 ${darkMode ? "text-white" : "text-black"}`}>	
             <Botao>
 
             <a
@@ -26,7 +46,7 @@ function App() {
                 href="https://www.instagram.com/sant0forte/?next=%2F" className="flex items-center gap-4 w-full h-full px-4 py-2 justify-center"
             >
                 <InstagramLogo size={32} />
-                <p>INSTAGRAM</p>
+                <p>Instagram</p>
             </a>
 
         </Botao>
@@ -34,10 +54,10 @@ function App() {
 
             <a
                 target="_blank"
-                href="https://www.instagram.com/sant0forte/?next=%2F" className="flex items-center gap-4 w-full h-full px-4 py-2 justify-center"
+                href="https://www.linkedin.com/in/joao-pedro-maciel-ferreira-3256b936a/" className="flex items-center gap-4 w-full h-full px-4 py-2 justify-center"
             >
                 <LinkedinLogo size={32} />
-                <p>INSTAGRAM</p>
+                <p>Linkedin</p>
             </a>
 
         </Botao>
@@ -45,10 +65,10 @@ function App() {
 
             <a
                 target="_blank"
-                href="https://www.instagram.com/sant0forte/?next=%2F" className="flex items-center gap-4 w-full h-full px-4 py-2 justify-center"
+                href="https://www.facebook.com/Joao.Pedro.mofi" className="flex items-center gap-4 w-full h-full px-4 py-2 justify-center"
             >
-                <InstagramLogo size={32} />
-                <p>INSTAGRAM</p>
+                <FacebookLogo size={32} />
+                <p>Facebook</p>
             </a>
 
         </Botao>
--- src/components/Botao/botao.jsx
+++ src/components/Botao/botao.jsx
@@ -1,10 +1,7 @@
-import { InstagramLogo } from "phosphor-react";
-
-function Botao(props) {
-    console.log(props);
+function Botao({ children, onClick }) {
     return (
-        <button className="cursor-pointer flex justify-center items-center gap-4 w-full rounded-lg border border-gray-400 overflow-hidden">
-            {props.children}
+        <button onClick={onClick} className="cursor-pointer flex justify-center items-center gap-4 w-full rounded-lg border border-gray-400 overflow-hidden">
+            {children}
         </button>
     );
 }
